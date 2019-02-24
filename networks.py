711497ed172aa291ad5c6e30f19a80431f75b3d7
JEONG SINYU <sinyu@ds.seoultech.ac.kr>
2019-02-24 10:51:05 +0900

    Update networks.py

--- networks.py
+++ networks.py
@@ -1,105 +1,109 @@
-import torch
-import torch.nn as nn
-import torch.nn.init as init
-import torch.nn.functional as F
-from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
-from scipy.stats import ortho_group
+import numpy as np
+import tensorflow as tf
+from module import BiGRU, Attention_, fc_, dirichlet_likelihood, cross_entropy
 
 
-class lda2vec_HAN_ver1(nn.Module):
-    def __init__(self, n_topics, vocab_size, embed_size, hidden_size, pre_embed=None, embed_fine_tune=True):
-        super(lda2vec_HAN_ver1, self).__init__()
-        self.HAN_hidden_size = hidden_size
-        self.LDA_hidden_size = hidden_size*2 # bidirectional encoding in HAN
-        self.HAN = HierarchicalAttentionNet(vocab_size, embed_size, self.HAN_hidden_size, pre_embed, embed_fine_tune)
+class LDA2vec_HAN(object):
+    def __init__(self, n_documents, vocab_size, embed_size, n_topics, pre_embed=True, embed_tuning=True, lda_dropout=0.3, lambda_=200, temperature=1.0):
+        assert embed_size % 2 == 0
+        tf.reset_default_graph()
 
-        # dirichlet loss
-        assert n_topics < hidden_size
-        self.n_topics = n_topics
-        #topic_matrix = ortho_group.rvs(self.LDA_hidden_size)[0:self.n_topics]
-        #self.topic_matrix = nn.Parameter(torch.FloatTensor(topic_matrix), requires_grad=True) # (hidden_size*2, n_topics)
-
-        self.topic_matrix = nn.Parameter(torch.empty(self.n_topics, self.LDA_hidden_size), requires_grad=True)
-        nn.init.orthogonal_(self.topic_matrix)
-
-        # mse loss
-        self.linear_ = nn.Linear(self.HAN_hidden_size*2, vocab_size)
-
-    def forward(self, x, sent_len, doc_len):
-        doc_vector = self.HAN(x, sent_len, doc_len) # (batch, hidden_size*2)
-
-        # dirichlet loss
-        doc_weight = torch.mm(doc_vector, torch.t(self.topic_matrix)) # (batch, n_topics)
-        doc_proportion = F.log_softmax(doc_weight, dim=1)
-
-        # mse loss
-        pred_proportion = F.softmax(self.linear_(doc_vector), dim=1) # (batch, vocab_size)
-
-        return doc_proportion, pred_proportion, doc_vector
-
-
-class AttentionLayer(nn.Module):
-    def __init__(self, hidden_size=200):
-        super(AttentionLayer, self).__init__()
-        self.hidden_size = hidden_size * 2
-        self.linear_ = nn.Linear(self.hidden_size, self.hidden_size)
-        self.tanh_ = nn.Tanh()
-        self.softmax_ = nn.Softmax(dim=1)
-
-    def forward(self, x):
-        u_context = torch.nn.Parameter(torch.FloatTensor(self.hidden_size).normal_(0, 0.01)).cuda()
-        h = self.tanh_(self.linear_(x)).cuda()
-        alpha = self.softmax_(torch.mul(h, u_context).sum(dim=2, keepdim=True))  # (x_dim0, x_dim1, 1)
-        attention_output = torch.mul(x, alpha).sum(dim=1)  # (x_dim0, x_dim2)
-        return attention_output, alpha
-
-
-class HierarchicalAttentionNet(nn.Module):
-    def __init__(self, vocab_size, embed_size, hidden_size, pre_embed=None, embed_finetune=True):
-        super(HierarchicalAttentionNet, self).__init__()
+        self.n_documents = n_documents
+        self.vocab_size = vocab_size
         self.embed_size = embed_size
-        self.hidden_size = hidden_size
-        # word level
-        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.hidden_size = embed_size // 2
+        self.n_topics = n_topics
+        self.pre_embed = pre_embed
+        self.embed_tuning = embed_tuning
+        self.lda_dropout = lda_dropout
+        self.lambda_ = lambda_
+        self.temperature = temperature
+        self.max_sent_length = 144
+        self.max_doc_length = 10
+
+        # HAN placeholder
+        self.batch_size = tf.placeholder(tf.int32, name='batch_size')
+        self.x = tf.placeholder(tf.int32, shape=[None, None, None], name='x')
+        self.y = tf.placeholder(tf.int32, shape=[None, vocab_size], name='y')
+        # LDA placeholder
+        self.pivot_idxs = tf.placeholder(tf.int32, shape=[None,], name='pivot_idxs')
+        self.target_idxs = tf.placeholder(tf.int32, shape=[None,], name='taregt_idxs')
+        self.doc_ids = tf.placeholder(tf.int32, shape=[None,], name='doc_ids')
+
+        # word vector
         if pre_embed:
-            self.weight = nn.Parameter(pre_embed, requires_grad=(True if embed_finetune else False))
+            han_embed = tf.Variable(tf.constant(0.0, shape=[self.vocab_size, self.embed_size]), trainable=self.embed_tuning, name='word_embed_HAN')
+            lda_embed = tf.Variable(tf.constant(0.0, shape=[self.vocab_size, self.embed_size]), trainable=self.embed_tuning, name='word_embed_LDA')
+            # self.han_embed_placeholder = tf.placeholder(tf.float32, [self.vocab_size, self.embed_size])
+            # self.lda_embed_placeholder = tf.placeholder(tf.float32, [self.vocab_size, self.embed_size])
+            # self.han_embed_init = han_embed.assign(self.han_embed_placeholder)
+            # self.lda_embed_init = lda_embed.assign(self.lda_embed_placeholder)
+            embed_init_vec = np.load('/home/datamininglab/Downloads/sinyu/embed.npy')
+            self.han_embed_init = tf.get_variable(name='word_embed_HAN', shape=[self.vocab_size, self.embed_size], initializer=tf.constant_initializer(embed_init_vec))
+            self.lda_embed_init = tf.get_variable(name='word_embed_LDA', shape=[self.vocab_size, self.embed_size], initializer=tf.constant_initializer(embed_init_vec))
         else:
-            init.normal_(self.embed.weight, std=0.01)
-            self.embed.weight.requires_grad = True
-        self.word_rnn = nn.GRU(embed_size, hidden_size, bidirectional=True)
-        self.word_att = AttentionLayer(hidden_size)
-        # sent level
-        self.sent_rnn = nn.GRU(hidden_size*2, hidden_size, bidirectional=True)
-        self.sent_att = AttentionLayer(hidden_size)
+            self.han_embed_init = tf.Variable(tf.truncated_normal((self.vocab_size, self.embed_size)), trainable=self.embed_tuning, name='word_embed_HAN')
+            init_width = 0.5 / self.embed_size
+            self.lda_embed_init = tf.Variable(tf.random_uniform([self.vocab_size, self.embed_size], -init_width, init_width), name='word_embed_LDA')
 
-    def forward(self, x, sent_lengths, doc_lengths):
-        # x shape -> (batch, max_doc, max_sent)
-        # 'sent_lengths', 'doc_lengths' must sorted in decreasing order
+        # HAN
+        with tf.variable_scope('HAN'):
+            doc_vec_HAN = self.HAN(self.han_embed_init)
 
-        assert x.shape[2] == max(sent_lengths)
-        assert x.shape[1] == max(doc_lengths)
-        max_sent_len = x.shape[2]
-        max_doc_len = x.shape[1]
+        # LDA2vec
+        with tf.variable_scope('LDA2vec'):
+            doc_weight, word_vec_LDA, doc_vec_LDA, topic_matrix = self.LDA2vec(self.lda_embed_init)
+            context = (tf.nn.dropout(word_vec_LDA, self.lda_dropout),
+                       tf.nn.dropout(doc_vec_LDA, self.lda_dropout))
+            context_vector = tf.add(*context)
 
-        # word embedding
-        word_embed = self.embed(x) # (batch, max_doc, max_sent, embed_size)
+        # nce loss
+        with tf.variable_scope('cross_entropy_loss'):
+            self.loss_ce = cross_entropy(word_vec_LDA, self.target_idxs, self.vocab_size)
 
-        # word encoding
-        word_embed = word_embed.view(-1, max_sent_len, self.embed_size)  # (-1(batch), max_sent_len, embed_size)
-        word_packed_input = pack_padded_sequence(word_embed, sent_lengths.cpu().numpy(), batch_first=True)
-        word_packed_output, _ = self.word_rnn(word_packed_input)
-        word_encode, _ = pad_packed_sequence(word_packed_output, batch_first=True) # (batch, max_sent_len, hidden_size*2)
+        # dirichlet loss
+        with tf.variable_scope('lda_loss'):
+            alpha = 1/self.n_topics
+            self.loss_lda = dirichlet_likelihood(doc_weight, self.n_topics, alpha) * self.lambda_
 
-        # word attention
-        sent_vector, sent_alpha = self.word_att(word_encode) # (batch, hidden_size*2)
+        # mse loss
+        with tf.variable_scope('mse_loss'):
+            merged_doc_vector = tf.add(doc_vec_LDA, doc_vec_HAN)
+            out = fc_(merged_doc_vector, self.vocab_size)
+            self.loss_mse = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.y, logits=out))
 
-        # sent encoding
-        sent_vector = sent_vector.view(-1, max_doc_len, self.hidden_size*2) # (batch, max_doc_len, hidden_size*2)
-        sent_packed_input = pack_padded_sequence(sent_vector, doc_lengths.cpu().numpy(), batch_first=True)
-        sent_packed_output, _ = self.sent_rnn(sent_packed_input)
-        sent_encode, _ = pad_packed_sequence(sent_packed_output, batch_first=True) # (batch, max_doc_len, hidden_size*2)
 
-        # sent attention
-        doc_vector, doc_alpha = self.sent_att(sent_encode) # (batch, hidden_size*2)
+    def HAN(self, han_embed):
+        with tf.variable_scope('word2vec'):
+            word_embed = tf.nn.embedding_lookup(han_embed, self.x)
+            word_embed = tf.reshape(word_embed, shape=[-1, self.max_sent_length, self.embed_size])
+        # sentence encoding
+        with tf.variable_scope('sent2vec'):
+            sent_encode = BiGRU(word_embed, self.hidden_size)
+            sent_attn = Attention_(sent_encode, self.hidden_size * 2)
+        # document encoding
+        with tf.variable_scope('doc2vec'):
+            sent_vector = tf.reshape(sent_attn, shape=[-1, self.max_doc_length, self.hidden_size*2])
+            doc_encode = BiGRU(sent_vector, self.hidden_size)
+            doc_attn = Attention_(doc_encode, self.hidden_size * 2)
+        return doc_attn
 
-        return doc_vector
+
+    def LDA2vec(self, lda_embed):
+        word_embed = tf.nn.embedding_lookup(lda_embed, self.pivot_idxs)
+
+        # document
+        scalar = 1 / np.sqrt(self.n_documents + self.n_topics)
+        doc_weight = tf.Variable(tf.random_normal([self.n_documents, self.n_topics], mean=0, stddev=50*scalar))
+        #unique_doc_ids, index_doc_ids = tf.unique(self.doc_ids)
+        #index_doc_ids = tf.unique_with_counts(index_doc_ids).count
+        #doc_weight_embed = tf.nn.embedding_lookup(doc_weight, unique_doc_ids)
+        doc_weight_embed = tf.nn.embedding_lookup(doc_weight, self.doc_ids)
+
+        doc_proportions = tf.nn.softmax(doc_weight_embed/self.temperature)
+        topic_matrix = tf.get_variable('topics', shape=(self.n_topics, self.embed_size), dtype=tf.float32, initializer=tf.orthogonal_initializer(gain=scalar))
+        topic_matrix = tf.nn.dropout(topic_matrix, self.lda_dropout)
+        doc_vector = tf.matmul(doc_proportions, topic_matrix)
+
+        return doc_weight_embed, word_embed, doc_vector, topic_matrix
+
