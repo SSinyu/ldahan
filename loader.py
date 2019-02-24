090af81a444b3ce01e986aa4e8ce16a4f119d609
JEONG SINYU <sinyu@ds.seoultech.ac.kr>
2019-02-24 10:51:22 +0900

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,68 +1,84 @@
-import pickle
 import numpy as np
-from collections import Counter
-from torch.utils.data import Dataset, DataLoader
+from operator import itemgetter
 
 
-class log_dataloader(Dataset):
-    def __init__(self, input_path, vocab_path, min_day_length, max_day_length, per_user, agg_min=10, target_time=3, mode='train'):
-        with open(input_path, 'rb') as f:
-            self.log_input = pickle.load(f)
-        with open(vocab_path, 'rb') as f:
-            self.vocab = pickle.load(f)
+class log_dataloader(object):
+    def __init__(self, log_seq, log_vocab, min_day, max_day, batch_size, window_size, target_hour):
+        self.log_seq = log_seq
+        self.log_vocab = log_vocab
+        self.min_day = min_day
+        self.max_day = max_day
+        self.batch_size = batch_size
+        self.window_size = window_size
+        self.target_len = target_hour * 6
+        self.day_log = 24 * 6
 
-        self.vocab_size = len(self.vocab)
-        self.min_day_length = min_day_length
-        self.max_day_length = max_day_length
-        self.per_user = per_user
-        self.target_length = target_time * 6 # (10m x 6 = 1hour)
-        self.day_length = (60 // agg_min) * 24
+    def get_batch(self, shuffle=True):
+        if shuffle:
+            indices = np.arange(len(self.log_seq))
+            np.random.shuffle(indices)
+            log_seq = itemgetter(*indices)(self.log_seq)
+        else:
+            log_seq = self.log_seq
 
-        self.mode = mode
+        for user_i in range(0, len(log_seq)-self.batch_size, self.batch_size):
+            batch_user = log_seq[user_i:(user_i+self.batch_size)]
 
-    def __len__(self):
-        return len(self.log_input)
+            #batch_hx, batch_y, batch_x, user_id = self.han_feed(batch_user)
+            #user_ids, pivot_logs, target_logs = self.lda_feed(user_id, batch_x)
+            #batch_hx, batch_y, pivot_logs, target_logs, user_ids = self.get_feed(batch_user)
+            batch_x, batch_hx, batch_y = [], [], []
+            user_ids, pivot_logs, target_logs = [], [], []
 
-    def __getitem__(self, idx):
-        user_log = self.log_input[idx]
+            for user_id, user_log in batch_user:
 
-        if self.mode == 'train':
-            batch_x, batch_y, day_lengths = [], [], []
-            for _ in range(self.per_user):
-                day_size = np.random.randint(self.min_day_length, self.max_day_length+1)
-                day_lengths.append(day_size)
+                day_length = np.random.randint(self.min_day, self.max_day + 1)
+                seq_size = day_length * self.day_log
+                x_start = np.random.randint(0, len(user_log) - seq_size - self.target_len)
+                x_end = x_start + seq_size
 
-                start_ind = np.random.randint(0, len(user_log) - self.day_length * day_size - self.target_length)
-                end_ind = start_ind + self.day_length * day_size
+                subseq_x = user_log[x_start:x_end]
+                # zero padding
+                subseq_x = subseq_x + [0] * (self.max_day * self.day_log - len(subseq_x))
+                # convert to be hierarchical
+                subseq_hx = [subseq_x[sub_i:(sub_i + self.day_log)] for sub_i in range(0, len(subseq_x), self.day_log)]
+                batch_hx.append(subseq_hx)
 
-                subset_x_max = [0 for _ in range(self.day_length * self.max_day_length)]
-                subset_x = user_log[start_ind:end_ind]
-                subset_x_max[0:len(subset_x)] = subset_x
-                subset_hx = [subset_x_max[sub_x_ind:(sub_x_ind+self.day_length)] for sub_x_ind in range(0, len(subset_x_max), self.day_length)]
-                batch_x.append(subset_hx)
+                y_end = x_end + self.target_len
+                subseq_y = user_log[x_end: y_end]  # x_end == y_start
+                batch_y.append(self.seq_to_ratio(subseq_y))
 
-                subset_y = user_log[end_ind:(end_ind+self.target_length)]
-                batch_y.append(subset_y)
-            return np.array(batch_x), self.seq_to_ratio(batch_y), np.array(day_lengths)
+                subseq_x = np.array(subseq_x)
+                subseq_x = subseq_x[np.where(subseq_x > 0)]
+                for pivot_i in range(self.window_size, len(subseq_x) - self.window_size):
+                    for wi in range(1, self.window_size+1):
+                        user_ids.append(user_id)
+                        pivot_logs.append(user_log[pivot_i])
+                        target_logs.append(user_log[pivot_i - wi])
+                        user_ids.append(user_id)
+                        pivot_logs.append(user_log[pivot_i])
+                        target_logs.append(user_log[pivot_i + wi])
 
-        elif self.mode == 'test':
-            hx = [user_log[x_ind:(x_ind+self.day_length)] for x_ind in range(0, len(user_log), self.day_length)]
-            day_len = [self.max_day_length]
-            return np.array(hx), np.array(day_len)
+            # sampling by batch size
+            total_ind = np.arange(len(user_ids))
+            np.random.shuffle(total_ind)
+            sltd_user_ids = itemgetter(*total_ind)(user_ids)[:self.batch_size]
+            sltd_pivot_logs = itemgetter(*total_ind)(pivot_logs)[:self.batch_size]
+            sltd_target_logs = itemgetter(*total_ind)(target_logs)[:self.batch_size]
 
-    def seq_to_ratio(self, seq):
-        ratio_list = []
-        for i, seq_3h in enumerate(seq):
-            counter_ = Counter(seq_3h).most_common(self.vocab_size)
-            labels = [0] * self.vocab_size
-            for ch, cnt in counter_:
-                labels[ch] = cnt/18
-            ratio_list.append(labels)
-        return np.array(ratio_list)
+            batch_hx = np.array(batch_hx)
+            batch_y = np.array(batch_y)
+            user_ids = np.array(sltd_user_ids)
+            pivot_logs = np.array(sltd_pivot_logs)
+            target_logs = np.array(sltd_target_logs)
 
+            yield batch_hx, batch_y, pivot_logs, target_logs, user_ids
 
-def get_loader(input_path, vocab_path, min_day_length=6, max_day_length=12, per_user=3, batch_size=5, agg_min=10, target_time=3, num_worker=7, mode='train'):
-    log_dataset = log_dataloader(input_path, vocab_path, min_day_length, max_day_length, per_user, agg_min, target_time, mode)
-    data_loader = DataLoader(dataset=log_dataset, batch_size=batch_size, shuffle=True, num_workers=num_worker)
-    return data_loader
+    def seq_to_ratio(self, y):
+        count_ = np.unique(y, return_counts=True)
+        ratio_ = [0] * len(self.log_vocab)
+        for ch, cnt in zip(count_[0], count_[1]):
+            ratio_[ch] = cnt/self.target_len
+        return ratio_
+
 
